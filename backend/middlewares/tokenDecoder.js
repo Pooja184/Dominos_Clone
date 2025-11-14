662d2a051a040153a4cbc94eadf886fe34fc2a1a
pxxja <poojabhambid2004@gmail.com>
2025-11-14 23:18:29 +0530

    token decoder func

---
+++ backend/middlewares/tokenDecoder.js
@@ -0,0 +1,27 @@
+import jwt from "jsonwebtoken";
+import User from "../models/userModel.js";
+
+export const protect = async (req, res, next) => {
+  try {
+    const token = req.cookies.token;
+
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: "Not authorized, no token",
+      });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = await User.findById(decoded.userId).select("-password");
+
+    next();
+
+  } catch (error) {
+    return res.status(401).json({
+      success: false,
+      message: "Token failed or expired",
+    });
+  }
+};
